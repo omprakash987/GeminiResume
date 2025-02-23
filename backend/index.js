cda7a9ba8062f76860c72f60b710f9378aed6204
omprakash parewa <kanuparewa371@gmail.com>
2025-02-23 19:00:45 +0530

    dirname updated in index.js

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ dotenv.config();
 const app = express(); 
 
 const PORT = 8000||process.env.PORT; 
+const __dirname = path.resolve(); 
 app.use(express.json({limit:'50mb'}));
 app.use(cookieParser()); 
 app.use(cors({
