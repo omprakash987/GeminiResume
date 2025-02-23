1902ac00b1f513aba9955b035b071dd320e86b4a
omprakash parewa <kanuparewa371@gmail.com>
2025-02-23 19:49:34 +0530

    port changed

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ import path from 'path'
 dotenv.config(); 
 const app = express(); 
 
-const PORT = process.env.PORT || 8000 
+const PORT = process.env.PORT || 5000 
 const __dirname = path.resolve(); 
 app.use(express.json({limit:'50mb'}));
 app.use(cookieParser()); 
